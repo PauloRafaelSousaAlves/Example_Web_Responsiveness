6a28a873995ee5a97b501ce55272f325cbc6b4e5
Paulo Rafael de Sousa Alves <paulo.alves.vortigo@superdigital.com.br>
2023-04-17 18:27:22 -0300

    initial appBar

---
+++ lib/pages/home/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:example_web_responsiveness/pages/home/widgets/appbar/mobile_appbar.dart';
+import 'package:example_web_responsiveness/pages/home/widgets/appbar/web_appbar.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          appBar: constraints.maxWidth < 800
+              ? const PreferredSize(
+                  preferredSize: Size(double.infinity, 56),
+                  child: MobileAppBar(),
+                )
+              : const PreferredSize(
+                  preferredSize: Size(double.infinity, 60),
+                  child: WebAppBar(),
+                ),
+          drawer: const Drawer(),
+        );
+      },
+    );
+  }
+}
